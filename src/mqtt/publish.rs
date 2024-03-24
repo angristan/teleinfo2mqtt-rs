b22d7f09d482c305f3be211c6633ea756fd31a1f
Stanislas Lange <git@slange.me>
2024-03-24 18:58:20 +0100

    Blink PiTInfo LED on message publish

--- src/mqtt/publish.rs
+++ src/mqtt/publish.rs
@@ -1,7 +1,11 @@
+use crate::teleinfo::parser::TeleinfoFrame;
 use aimeqtt::client::Client;
+use rppal::gpio::Gpio;
+use std::thread;
+use std::time::Duration;
 use tokio::task;
 
-use crate::teleinfo::parser::TeleinfoFrame;
+const GPIO_PITINFO_GREEN_LED: u8 = 4;
 
 pub fn publish_teleinfo(client: &Client, value: TeleinfoFrame) {
     let client_clone = (*client).clone();
@@ -10,8 +14,20 @@ pub fn publish_teleinfo(client: &Client, value: TeleinfoFrame) {
             client_clone.publish(format!("teleinfo/{}", value.adco), value.to_string());
 
         match publish_res {
-            Ok(_) => println!("Published"),
-            Err(e) => println!("Error: {:?}", e),
+            Ok(_) => {
+                println!("Published MQTT message");
+
+                let mut pin = Gpio::new()
+                    .unwrap()
+                    .get(GPIO_PITINFO_GREEN_LED)
+                    .unwrap()
+                    .into_output();
+
+                pin.set_high();
+                thread::sleep(Duration::from_millis(10));
+                pin.set_low();
+            }
+            Err(e) => eprintln!("Error: {:?}", e),
         }
     });
 }
